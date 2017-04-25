da90ad9c7d4f5e3c2fb12e2a618f03be25ff8464
ajinkya12 <dajinkya.12@gmail.com>
2017-04-25 22:36:17 +0530

    debug 10

--- main.py
+++ main.py
@@ -18,7 +18,34 @@ def SlackGet():
 @app.route("/slack", methods=['POST'])
 def slackReq(): 
 	req_data = request.form
-	return req_data['channel_id']
+	channel_id = req_data['channel_id']
+	response =  slack.channels.history(channel_id)
+	a = (response.body)
+	para = ""
+	concepts = ""
+	for i in range(len(a['messages']) - 1, -1, -1):
+		para += a['messages'][i]['text'] + ". "
+	para = para.decode("utf-8")
+	print para
+	#Use your own api key
+	payload = {'apikey': 'a429a338-07a1-4b6e-bd46-c75b1fab8c89', 'text': para}
+	r = requests.get('http://api.idolondemand.com/1/api/sync/extractconcepts/v1', params=payload)
+	json_r = json.loads(r.text)
+	for i in range(len(json_r['concepts'])/2):
+		temp = json_r['concepts'][i]['concept']
+		if (len(temp) >= 4) and (" " in temp) and (temp != "joined the channel"):
+			concepts += temp + ", "
+
+	summary_token = textrank(para)
+	summary = ""
+	for i in summary_token:
+		if "has joined the channel" not in i:
+			summary += i + " "
+
+	res = "*Chat Summary:* \n " + summary + "\n \n" + "*Topics Discussed:*  \n" + concepts
+
+	print res
+	return str(res)
 
 if __name__ == "__main__":
 	# Bind to PORT if defined, otherwise default to 5000.
